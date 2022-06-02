94b00c45dda713d950d238cb12d821d42a41490f
Toby Dossin <tdossin@moderncampus.com>
2022-06-02 10:37:40 -0400

    hoisting scrape variables to top of script

--- decoix.mjs
+++ decoix.mjs
@@ -1,13 +1,15 @@
-// Handy Dandy Website Scraper
-
-const scrapeTarget = "/"; // This is the URL for the page you want to duplicate
-
-const scrapeToDirectory = "./_ORIG"; // This is the local folder for the website files
+// Decoix&copy;
 
 import scrape from "website-scraper";
 import * as plugins from "website-scraper/plugins";
+//import PuppeteerPlugin from "website-scraper-puppeteer"; // not sure if we need this yet
 import liveServer from "live-server";
 import path from "path";
+
+const scrapeTarget = "https://www.yale.edu/"; // URL for the page you want to duplicate
+const scrapeToDirectory = "./_ORIG_NOPLUGIN"; // Local folder for the website files
+const decoixHost = "127.0.0.2"; // Host to use for local website
+const decoixPort = "8888"; // Port to use for local website
 
 // Use Node to run the scraper script by typing
 // > node scraper.mjs
@@ -15,18 +17,29 @@ import path from "path";
 const scrapeOptions = {
   urls: [scrapeTarget],
   directory: scrapeToDirectory,
+  recursive: false,
+  maxRecursiveDepth: null,
   subdirectories: [
-    { directory: "img", extensions: [".jpg", ".png", ".svg"] },
+    { directory: "", extensions: [".html", ".ico"] },
+    { directory: "img", extensions: [".jpg", ".png", ".svg", ".gif"] },
     { directory: "vid", extensions: [".mp4"] },
     { directory: "js", extensions: [".js"] },
     { directory: "css", extensions: [".css"] },
     { directory: "fonts", extensions: [".woff", ".woff2", ".eot", ".ttf"] },
+    { directory: "orphans", extensions: ["."] },
   ],
+  // plugins: [
+  //   new PuppeteerPlugin({
+  //     launchOptions: { headless: false } /* optional */,
+  //     scrollToBottom: { timeout: 10000, viewportN: 10 } /* optional */,
+  //     blockNavigation: false /* optional */,
+  //   }),
+  // ],
 };
 
 const serverParams = {
-  port: 8888, // Set the server port
-  host: "127.0.0.2", // Set the address to use for the local server
+  host: decoixHost, // Set the address to use for the local server
+  port: decoixPort, // Set the server port
   root: scrapeToDirectory, // Set root directory that's being served
   wait: 1000, // Waits for all changes, before reloading (ms)
 };
